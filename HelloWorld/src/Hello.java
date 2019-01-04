9895231568c3651a7bad35674e1bf11632cc8bb5
Paul Allen <p.allen.art@gmail.com>
2019-01-03 22:42:18 -0700

    number data types.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,5 +4,18 @@ public class Hello {
 
         System.out.println("Hello World!");
 
+        // int width of 32
+        int minValue = -2_147_483_647;
+        int maxValue = 2_147_483_647;
+
+        // byte width of 8
+        byte myByte = 127;
+
+        // short width of 16
+        short shortValue = 32767;
+
+        // long has a width of 64
+        long longValue = 10000000L;
     }
+
 }
