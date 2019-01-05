bf2802ebb24cc991760acfacc8f1a05d4f0a1d37
Paul Allen <p.allen.art@gmail.com>
2019-01-04 22:56:43 -0700

    Keywords.

---
+++ KeyWords/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Expression is everything after the data type.
+        // data type / expression
+        float myNumber = 77;
+
+        // Expression is in the brackets
+        if ( myNumber > 70 ) {
+            System.out.println("Hello World");
+        }
+
+    }
+}
